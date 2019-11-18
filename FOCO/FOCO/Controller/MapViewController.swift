f25c2d52c47032fd0c136d30207744e252084d9b
Beatriz <bivilin@gmail.com>
2019-11-18 11:39:49 -0300

    Adding custom glyphs to disease marker, setting "under construction" alert for non-implemented features and adding dangerous areas surrounding disease markers.

--- FOCO/FOCO/Controller/MapViewController.swift
+++ FOCO/FOCO/Controller/MapViewController.swift
@@ -14,6 +14,7 @@ class MapViewController: UIViewController {
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var newInputButton: UIView!
     fileprivate var points: [DiseaseAnnotation]?
+    var dangerousAreas: [MKOverlay] = [MKOverlay]()
     let regionRadius: CLLocationDistance = 400
     let locationManager = CLLocationManager()
     let maxSpan = MKCoordinateSpan(latitudeDelta: 0.03, longitudeDelta: 0.03)
@@ -33,19 +34,22 @@ class MapViewController: UIViewController {
 
     // Loads initial markers
     func loadInitialData() {
-        // Loads disease occurences
+        // Loads disease occurences and coverage area
         DiseaseOccurrencesServices.getAllDiseases { (errorMessage, points) in
             if let data = points {
                 // Maps occurrences to annotations
                 self.points = data.map { (diseaseOccurrence) -> DiseaseAnnotation in
                     // The magic happens here
                     let annotation = DiseaseAnnotation(disease: diseaseOccurrence)
+                    self.dangerousAreas.append(MKCircle(center: annotation.coordinate, radius: 100))
                     return annotation
                 }
-                // Adds annotations to map view
+
+                // Adds annotations and overlays to map view
                 OperationQueue.main.addOperation {
                     if let data = self.points {
                         self.mapView.addAnnotations(data)
+                        self.mapView.addOverlays(self.dangerousAreas)
                     }
                 }
             } else {
@@ -53,7 +57,7 @@ class MapViewController: UIViewController {
             }
         }
     }
-    
+
     @IBAction func newInputClick(_ sender: Any) {
         alertUnderConstruction()
     }
@@ -61,6 +65,19 @@ class MapViewController: UIViewController {
 
 // MARK: Map View Delegate
 extension MapViewController: MKMapViewDelegate {
+
+    // Renders Map overlays
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+
+        if overlay is MKCircle {
+            let circleRender = MKCircleRenderer(overlay: overlay)
+            circleRender.fillColor = UIColor(hue: 9/360, saturation: 66/100, brightness: 92/100, alpha: 0.5)
+            circleRender.lineWidth = 10
+
+            return circleRender
+        }
+        return MKPolylineRenderer()
+    }
 
     // Reuses annotations
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
@@ -75,10 +92,13 @@ extension MapViewController: MKMapViewDelegate {
           dequeuedView.annotation = annotation
           view = dequeuedView
         } else {
-          view = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: identifier)
-          view.canShowCallout = true
-          view.calloutOffset = CGPoint(x: -5, y: 5)
-          view.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+            view = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+            view.glyphImage = UIImage(named: "sick")
+            view.markerTintColor = UIColor(red: 249/255, green: 220/255, blue: 29/255, alpha: 1)
+            view.glyphTintColor = .black
+            view.canShowCallout = true
+            view.calloutOffset = CGPoint(x: -5, y: 5)
+            view.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
         }
         return view
     }
@@ -93,9 +113,9 @@ extension MapViewController: MKMapViewDelegate {
     }
 
     func alertUnderConstruction() {
-        let alert = UIAlertController(title: "Not Yet!", message: "We are working on developing this tool.", preferredStyle: .alert)
+        let alert = UIAlertController(title: "Soon!", message: "Under construction.", preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { action in
-            switch action.style{
+            switch action.style {
             case .default:
                 print("default")
             case .cancel:
@@ -107,6 +127,7 @@ extension MapViewController: MKMapViewDelegate {
     }
 }
 
+// MARK: CoreLocation Delegate
 extension MapViewController: CLLocationManagerDelegate {
     func setupLocationServices() {
         self.locationManager.delegate = self
